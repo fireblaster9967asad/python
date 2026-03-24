d8b1732201f57a67040f3d49ca6f06d54aa96392
fireblaster9967asad <adrshazia@gmail.com>
2026-03-24 20:12:31 +0500

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,15 @@
+Age = 67
+
+weight = 67.6767
+
+name = "sixseven"
+
+isfemale = True
+
+print("type of weight is",type(weight))
+
+print("type of name is",type(name))
+
+print("type of Age is",type(Age))
+
+print("type of isfemale is",type(isfemale))
---
+++ typecasting.py
@@ -0,0 +1,13 @@
+age = 67
+
+print("type before casting")
+print("type of age", type(age))
+
+newage = float (age)
+
+print("after typecasting")
+
+print("type of age is",type(newage))
+
+
+
