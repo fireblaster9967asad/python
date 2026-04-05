1e42a89557a3ddd495a398c7ae5126d8c64d9835
fireblaster9967asad <adrshazia@gmail.com>
2026-04-05 13:12:22 +0500

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,9 @@
+amount = 1560
+
+note_1 = amount //100
+note_2 = (amount % 100) // 50
+note_3 = ((amount%100)) % 50 // 10
+
+print("notes of 100 rupees",note_1)
+print("notes of 50 rupees",note_2)
+print("notes of 10 rupees",note_3)
