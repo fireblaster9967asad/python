728a20f272146c72d864160b19f515dee5ac4b6e
fireblaster9967asad <adrshazia@gmail.com>
2026-04-12 13:14:41 +0500

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,17 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("all numbers have boolean value as true")
+else:
+    print("atleast one number has boolean value false")
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
---
+++ notequal.py
@@ -0,0 +1,27 @@
+a = 67
+b = 911
+c = 911
+
+print(a != b)
+print(b == c)
+
+a = "python"
+b = "coding"
+
+if a != b :
+    print(a,'and',b, 'are different')
+
+a = 4
+b = 5
+
+if False != True :
+    print("Hola sai dora")
+
+a = int(input("entrt a number"))
+
+if a%2 != 0 :
+    print(a,"is not even a number")
+
+
+
+
